39e1d81f0b62aa3e5d3e29c2556b1677a257695f
pavithrachandran70 <pavithra.chandran@zemosolabs.com>
2025-07-25 10:26:10 +0530

    bookservice

---
+++ bookservice/src/main/java/com/example/bookservice/exception/ErrorResponse.java
@@ -0,0 +1,22 @@
+package com.example.bookservice.exception;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+
+@Getter
+@Setter
+public class ErrorResponse {
+    private LocalDateTime timestamp;
+    private String message;
+    private int status;
+
+    public ErrorResponse(LocalDateTime timestamp, String message, int status) {
+        this.timestamp = timestamp;
+        this.message = message;
+        this.status = status;
+    }
+
+}
